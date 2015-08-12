1649c68a0bc4a327f19cd1113b76872c7075261c
dcolclazier <david@jdcnetworksolutions.com>
2015-08-12 12:48:53 -0600

    More updates - nothing of note

---
+++ Assets/Scripts/GuiManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class GuiManager :MonoBehaviour
+{
+    public void exit_click()
+    {
+        Debug.Log("Exit button clicked!");
+        Application.Quit();
+    }
+
+    public void singlep_click()
+    {
+        Debug.Log("Single player button clicked.");
+    }
+
+    public void multip_click()
+    {
+        Debug.Log("Multiplayer button clicked.");
+    }
+
+    public void options_click()
+    {
+        Debug.Log("Options button clicked.");
+    }
+
+
+}
