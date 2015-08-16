c8baeaf6b91a478f358302c1397051b080494cdc
dcolclazier <david@jdcnetworksolutions.com>
2015-08-15 23:39:06 -0600

    general cleanup

--- Assets/Code/Statics/NodeManager.cs
+++ Assets/Code/Statics/NodeManager.cs
@@ -9,7 +9,7 @@ namespace Assets.Code
 {
     public static class NodeManager
     {
-        public static Dictionary<Entity, List<Node>> EntityNodes;
+        public static readonly Dictionary<Entity, List<Node>> EntityNodes;
         //this code kinda sucks... maybe refactor it?
         //1 - static classes blow - global state!
         //2 - repeating if's? redundant poly-casts? I know there's only 4 cases, but it's like... c'mon.
@@ -36,8 +36,6 @@ namespace Assets.Code
                     var angle = radians*(i+1);
                     var xMag = Mathf.Round(Mathf.Cos(angle)*1000f)/1000f;
                     var yMag = Mathf.Round(Mathf.Sin(angle)*1000f)/1000f;
-                    Debug.Log(string.Format("Angle: {0} .. Radius: {1} Cos(angle): {2}  Sin(angle): {3}",
-                                            angle, circlecheck.radius, xMag, yMag));
                     yield return new Node(new Vector2(xMag,yMag));
                 }
             }
@@ -62,17 +60,16 @@ namespace Assets.Code
             }
             
         }
-
-        //returns an IEnumerable containing every node of "Solid" objects
-        //currently does not return the entity associated with the nodes, but
-        //that can be added very easily if need-be
         public static IEnumerable<Node> GetSolidNodes() {
 
             return EntityNodes.Where(item => item.Key.Solid).SelectMany(entity => entity.Value);
         }
 
         public static void Cleanup() {
-            //clean up for scene change
+            foreach (var key in EntityNodes) {
+                key.Value.Clear();
+            }
+            EntityNodes.Clear();
         }
     }
 }
