7e97e5babad2f7bc1fae958628e95ebd52aae874
dcolclazier <david@jdcnetworksolutions.com>
2015-08-22 23:52:36 -0600

    Trying to fix stuff

--- Assets/Code/Entities/Components/BasicAttackCompononent.cs
+++ Assets/Code/Entities/Components/BasicAttackCompononent.cs
@@ -2,9 +2,8 @@ using System.Collections.Generic;
 using Assets.Code.Abstract;
 using UnityEngine.UI;
 using UnityEngine;
-using Assets.Code.Abstract;
 using Assets.Code.Statics;
-
+using Vectrosity;
 
 
 namespace Assets.Code.Entities.Components {
@@ -15,6 +14,11 @@ namespace Assets.Code.Entities.Components {
 		private GameObject button;
 		private Sprite defaultSprite;
 		private bool enabled;
+		private VectorLine rangeCircle;
+
+		private float range = 1.2f;
+		private int damage = 1;
+		private float _lineThickness = 2.2f;
 
 		public List<string> Dependencies {
 			get { return new List<string>() {
@@ -30,11 +34,23 @@ namespace Assets.Code.Entities.Components {
 			button = GameObject.Find("Button_" + AbilityNum);
 			defaultSprite = button.GetComponent<Image>().sprite;
 			Messenger.AddListener<GameObject>("GameObjectSelected", OnSelected);
+<<<<<<< HEAD
 			Messenger.AddListener<Vector2>("GameObjectDeselected", OnDeselect); //method of deselecting should change
 			//Messenger.AddListener<?>("Ability#3Clicked", IAmActivated);
 			//Messenger.AddListener<Entity>("AttackableEnemyClicked", DoAttack);
 
             //Messenger.AddListener<LayerFlag, RaycastHit2D>("LeftMouseDown");
+=======
+			Messenger.AddListener<Vector2>("GroundClicked", OnDeselect); //method of deselecting should change
+			Messenger.AddListener("AbilityOneClicked", IAmActivated);
+			Messenger.AddListener("MouseEnterAbility1", mouseEnter);
+			Messenger.AddListener("MouseExitAbility1", mouseExit);
+			//Messenger.AddListener<Entity>("AttackableEnemyClicked", DoAttack);
+			rangeCircle = new VectorLine("Select Circle", new Vector3[720], null, _lineThickness);
+			rangeCircle.Draw3DAuto();
+			rangeCircle.color = Color.green;
+			//VectorLine.canvas3D.sortingLayerName = "Select Circles";
+>>>>>>> 4c14bffd4583ba7a57140995a82a0db2f22474b5
 
 		}
 		private void OnSelected(GameObject selectedObj)
@@ -56,10 +72,14 @@ namespace Assets.Code.Entities.Components {
 		}
 		private void IAmActivated()
 		{
-			//Activated = true;
-			//draw gold box around ability
-			//show range of ability even without mouse hovering over, from unit current local
-			//change enemies in range tint red to show targetable
+			if (enabled)
+			{
+				Debug.Log("############## BASIC ATTACK ACTIVATED ###############");
+				//Activated = true;
+				//draw gold box around ability
+				//show range of ability even without mouse hovering over, from unit current local
+				//change enemies in range tint red to show targetable
+			}
 		}
 		private void DoAttack()
 		{
@@ -68,6 +88,26 @@ namespace Assets.Code.Entities.Components {
 			//broadcast X damage done to X Entity
 			//play animation
 		}
+		private void mouseEnter()
+		{
+			Debug.Log("Ability On Mouse over is being called");
+			if (enabled)
+			{
+				//should call this stuff from an external function probably
+				Vector2 whereMe = Parent.Attributes.Get<Vector2>("CurrentPathTarget");
+				if (whereMe == null) whereMe = Parent.Attributes.Get<Vector2>("Position");
+				rangeCircle.MakeCircle(whereMe, range);
+				rangeCircle.active = true;
+				//see if enemy units would be in range
+				Messenger.Broadcast("AttackableInRange", whereMe, range);
+				//tooltip popup
+			}
+		}
+		private void mouseExit()
+		{
+			rangeCircle.active = false;
+		}
+
 		public Entity Parent { get; set; }
 	}
 }
